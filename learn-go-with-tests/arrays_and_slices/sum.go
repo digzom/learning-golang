8623e16015320e48964d5ecef801c113167a7efa
Dickson <dicksonmelo@gmail.com>
2024-08-17 23:41:05 -0300

    feat(arrays_and_slices): Add SumAll and SumAllTails functions with corresponding tests

--- learn-go-with-tests/arrays_and_slices/sum.go
+++ learn-go-with-tests/arrays_and_slices/sum.go
@@ -18,6 +18,33 @@ func Sum2(numbers []int) int {
 	return sum
 }
 
+func SumAll(numbersToSum ...[]int) []int {
+	// lengthOfNumbers := len(numbersToSum)
+	// this creates a slice object
+	// sums := make([]int, lengthOfNumbers)
+	var sums []int
+
+	for _, numbers := range numbersToSum {
+		sums = append(sums, Sum2(numbers))
+	}
+
+	return sums
+}
+
+func SumAllTails(numbersToSum ...[]int) []int {
+	var sums []int
+	for _, numbers := range numbersToSum {
+		if len(numbers) >= 1 {
+			tail := numbers[1:]
+			sums = append(sums, Sum2(tail))
+		} else {
+			sums = append(sums, 0)
+		}
+	}
+
+	return sums
+}
+
 func main() {
 	Sum([5]int{1, 2, 3, 4, 5})
 }
--- learn-go-with-tests/arrays_and_slices/sum_test.go
+++ learn-go-with-tests/arrays_and_slices/sum_test.go
@@ -1,6 +1,9 @@
 package sum
 
-import "testing"
+import (
+	"slices"
+	"testing"
+)
 
 func TestSum(t *testing.T) {
 	t.Run("array of 5 numbers", func(t *testing.T) {
@@ -21,9 +24,43 @@ func TestSum2(t *testing.T) {
 
 		got := Sum2(numbers)
 		want := 21
-
-		if got != want {
-			t.Errorf("got %d want %d given, %v", got, want, numbers)
-		}
+		assertIntSums(t, numbers, got, want)
 	})
+}
+
+func TestSumAll(t *testing.T) {
+	t.Run("varying number of slices", func(t *testing.T) {
+		got := SumAll([]int{1, 2}, []int{0, 9})
+		want := []int{3, 9}
+
+		assertSums(t, got, want)
+	})
+}
+
+func TestSumAllTails(t *testing.T) {
+	t.Run("return the sum of the tails of slices", func(t *testing.T) {
+		got := SumAllTails([]int{1, 2, 3, 5}, []int{4, 33, 29, 1}, []int{49, 3, 0, 22}, []int{1, 2})
+		want := []int{10, 63, 25, 2}
+
+		assertSums(t, got, want)
+	})
+
+	t.Run("returns zero when dealing with empty slice", func(t *testing.T) {
+		got := SumAllTails([]int{}, []int{4, 5, 3})
+		want := []int{0, 8}
+
+		assertSums(t, got, want)
+	})
+}
+
+func assertSums(t *testing.T, got, want []int) {
+	if !slices.Equal(got, want) {
+		t.Errorf("got %v want %v", got, want)
+	}
+}
+
+func assertIntSums(t *testing.T, numbers []int, got, want int) {
+	if got != want {
+		t.Errorf("got %d want %d given, %v", got, want, numbers)
+	}
 }
